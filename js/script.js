ccc6bcd26cf1300245f719053df5333a59bce4bb
Greg Herbowicz <herbowicz@gmail.com>
2019-06-16 12:46:50 +0200

    proper sizes

--- js/script.js
+++ js/script.js
@@ -1,117 +1,117 @@
 let sizes = [{
     "Social": [
-        { "name": "FB Page Cover", "width": 1640, "height": 664 },
-        { "name": "FB Shared Image", "width": 1200, "height": 630 },
-        { "name": "FB Event Image", "width": 1920, "height": 1080 },
+        { "name": "FB Page Cover", "width": 1640, "height": 664, unit: "px" },
+        { "name": "FB Shared Image", "width": 1200, "height": 630, unit: "px" },
+        { "name": "FB Event Image", "width": 1920, "height": 1080, unit: "px" },
 
-        { "name": "FB Group Header", "width": 1640, "height": 856 },
-        { "name": "Instagram", "width": 1080, "height": 1080 },
-        { "name": "YouTube Profile", "width": 800, "height": 800 },
+        { "name": "FB Group Header", "width": 1640, "height": 856, unit: "px" },
+        { "name": "Instagram", "width": 1080, "height": 1080, unit: "px" },
+        { "name": "YouTube Profile", "width": 800, "height": 800, unit: "px" },
 
-        { "name": "YouTube Cover", "width": 2560, "height": 1440 },
-        { "name": "Twitter Profile", "width": 400, "height": 400 },
-        { "name": "Twitter Cover", "width": 1500, "height": 500 }
-    ]
+        { "name": "YouTube Cover", "width": 2560, "height": 1440, unit: "px" },
+        { "name": "Twitter Profile", "width": 400, "height": 400, unit: "px" },
+        { "name": "Twitter Cover", "width": 1500, "height": 500, unit: "px" }
+    ],
 },
 {
     "Screen": [
-        { "name": "VGA", "width": 640, "height": 480 },
-        { "name": "XGA", "width": 1024, "height": 768 },
-        { "name": "HD", "width": 1280, "height": 720 },
+        { "name": "VGA", "width": 640, "height": 480, unit: "px" },
+        { "name": "XGA", "width": 1024, "height": 768, unit: "px" },
+        { "name": "HD", "width": 1280, "height": 720, unit: "px" },
 
-        { "name": "", "width": 1366, "height": 768 },
-        { "name": "", "width": 1600, "height": 900 },
-        { "name": "Full HD", "width": 1920, "height": 1080 },
+        { "name": "", "width": 1366, "height": 768, unit: "px" },
+        { "name": "", "width": 1600, "height": 900, unit: "px" },
+        { "name": "Full HD", "width": 1920, "height": 1080, unit: "px" },
 
-        { "name": "2K", "width": 2048, "height": 1080 },
-        { "name": "UHD", "width": 3840, "height": 2160 },
-        { "name": "4K", "width": 4096, "height": 2160 },
-    ]
+        { "name": "2K", "width": 2048, "height": 1080, unit: "px" },
+        { "name": "UHD", "width": 3840, "height": 2160, unit: "px" },
+        { "name": "4K", "width": 4096, "height": 2160, unit: "px" },
+    ],
 },
 {
     "Ads": [
-        { "name": "Small Square", "width": 200, "height": 200 },
-        { "name": "Square", "width": 250, "height": 250 },
-        { "name": "Medium Rect", "width": 300, "height": 250 },
+        { "name": "Small Square", "width": 200, "height": 200, unit: "px" },
+        { "name": "Square", "width": 250, "height": 250, unit: "px" },
+        { "name": "Medium Rect", "width": 300, "height": 250, unit: "px" },
 
-        { "name": "Large Rect", "width": 336, "height": 280 },
-        { "name": "Mobile Leaderboard", "width": 320, "height": 50 },
-        { "name": "Large Mobile", "width": 320, "height": 100 },
+        { "name": "Large Rect", "width": 336, "height": 280, unit: "px" },
+        { "name": "Mobile Leaderboard", "width": 320, "height": 50, unit: "px" },
+        { "name": "Large Mobile", "width": 320, "height": 100, unit: "px" },
 
-        { "name": "Banner", "width": 468, "height": 60 },
-        { "name": "Leaderboard", "width": 728, "height": 90 },
-        { "name": "Large Leaderboard", "width": 970, "height": 90 },
+        { "name": "Banner", "width": 468, "height": 60, unit: "px" },
+        { "name": "Leaderboard", "width": 728, "height": 90, unit: "px" },
+        { "name": "Large Leaderboard", "width": 970, "height": 90, unit: "px" },
 
-        { "name": "Billboard", "width": 970, "height": 250 },
-        { "name": "Vertical Rect", "width": 240, "height": 400 },
-        { "name": "Skyscrapper", "width": 120, "height": 600 },
+        { "name": "Billboard", "width": 970, "height": 250, unit: "px" },
+        { "name": "Vertical Rect", "width": 240, "height": 400, unit: "px" },
+        { "name": "Skyscrapper", "width": 120, "height": 600, unit: "px" },
 
-        { "name": "Wide Skyscraper", "width": 160, "height": 600 },
-        { "name": "Half Page", "width": 300, "height": 600 },
-        { "name": "Portrait", "width": 300, "height": 1050 },
+        { "name": "Wide Skyscraper", "width": 160, "height": 600, unit: "px" },
+        { "name": "Half Page", "width": 300, "height": 600, unit: "px" },
+        { "name": "Portrait", "width": 300, "height": 1050, unit: "px" },
 
-    ]
+    ],
 },
 {
     "Mobile": [
-        { "name": "iPhone 4", "width": 640, "height": 900 },
-        { "name": "iPhone 5", "width": 640, "height": 1136 },
-        { "name": "iPhone 6 7 8", "width": 750, "height": 1334 },
+        { "name": "iPhone 4", "width": 640, "height": 900, unit: "px" },
+        { "name": "iPhone 5", "width": 640, "height": 1136, unit: "px" },
+        { "name": "iPhone 6 7 8", "width": 750, "height": 1334, unit: "px" },
 
-        { "name": "iPhone 6+ 7+ 8+", "width": 1080, "height": 1920 },
-        { "name": "iPhone X", "width": 1125, "height": 2436 },
-        { "name": "Samsung A5", "width": 720, "height": 1280 },
+        { "name": "iPhone 6+ 7+ 8+", "width": 1080, "height": 1920, unit: "px" },
+        { "name": "iPhone X", "width": 1125, "height": 2436, unit: "px" },
+        { "name": "Samsung A5", "width": 720, "height": 1280, unit: "px" },
 
-        { "name": "Samsung S5", "width": 1080, "height": 1920 },
-        { "name": "Samsung S6", "width": 1440, "height": 2560 },
-        { "name": "Samsung S8+", "width": 1440, "height": 2560 },
+        { "name": "Samsung S5", "width": 1080, "height": 1920, unit: "px" },
+        { "name": "Samsung S6", "width": 1440, "height": 2560, unit: "px" },
+        { "name": "Samsung S8+", "width": 1440, "height": 2560, unit: "px" },
 
-        { "name": "Google Pixel", "width": 1080, "height": 1920 },
-        { "name": "iPad Mini 4", "width": 1536, "height": 2048 },
-        { "name": "iPad Air 2", "width": 1536, "height": 2048 },
+        { "name": "Google Pixel", "width": 1080, "height": 1920, unit: "px" },
+        { "name": "iPad Mini 4", "width": 1536, "height": 2048, unit: "px" },
+        { "name": "iPad Air 2", "width": 1536, "height": 2048, unit: "px" },
 
-        { "name": "iPad Pro 9.7''", "width": 1536, "height": 2048 },
-        { "name": "iPad Pro 10.5''", "width": 1668, "height": 2224 },
-        { "name": "iPad Pro 12.9''", "width": 2048, "height": 2732 }
-    ]
+        { "name": "iPad Pro 9.7''", "width": 1536, "height": 2048, unit: "px" },
+        { "name": "iPad Pro 10.5''", "width": 1668, "height": 2224, unit: "px" },
+        { "name": "iPad Pro 12.9''", "width": 2048, "height": 2732, unit: "px" }
+    ],
 },
 {
     "Squares": [
-        { "name": "", "width": 16, "height": 16 },
-        { "name": "", "width": 32, "height": 32 },
-        { "name": "", "width": 64, "height": 64 },
-        { "name": "", "width": 128, "height": 128 },
-        { "name": "", "width": 256, "height": 256 },
-        { "name": "", "width": 512, "height": 512 },
-        { "name": "", "width": 1024, "height": 1024 },
-        { "name": "", "width": 2048, "height": 2048 },
-        { "name": "", "width": 4096, "height": 4096 },
+        { "name": "", "width": 16, "height": 16, unit: "px" },
+        { "name": "", "width": 32, "height": 32, unit: "px" },
+        { "name": "", "width": 64, "height": 64, unit: "px" },
+        { "name": "", "width": 128, "height": 128, unit: "px" },
+        { "name": "", "width": 256, "height": 256, unit: "px" },
+        { "name": "", "width": 512, "height": 512, unit: "px" },
+        { "name": "", "width": 1024, "height": 1024, unit: "px" },
+        { "name": "", "width": 2048, "height": 2048, unit: "px" },
+        { "name": "", "width": 4096, "height": 4096, unit: "px" },
 
-    ]
+    ],
 },
 {
     "Print": [
-        { "name": "A3", "width": 297, "height": 420 },
-        { "name": "A4", "width": 210, "height": 297 },
-        { "name": "A5", "width": 297, "height": 420 },
-        { "name": "A6", "width": 210, "height": 297 },
-        { "name": "B3", "width": 297, "height": 420 },
-        { "name": "B4", "width": 210, "height": 297 },
-        { "name": "B5", "width": 297, "height": 420 },
-        { "name": "Business Card", "width": 3.5, "height": 2 },
-    ]
+        { "name": "A3", "width": 297, "height": 420, unit: "mm" },
+        { "name": "A4", "width": 210, "height": 297, unit: "mm" },
+        { "name": "A5", "width": 148, "height": 210, unit: "mm" },
+        { "name": "A6", "width": 105, "height": 148, unit: "mm" },
+        { "name": "B3", "width": 353, "height": 500, unit: "mm" },
+        { "name": "B4", "width": 250, "height": 353, unit: "mm" },
+        { "name": "B5", "width": 176, "height": 250, unit: "mm" },
+        { "name": "Business Card", "width": 3.5, "height": 2, unit: "in" },
+    ],
 },
 {
     "Photo": [
-        { "name": "Wallet", "width": 2, "height": 3 },
-        { "name": "Enprint", "width": 3.5, "height": 5 },
-        { "name": "", "width": 5, "height": 7 },
-        { "name": "", "width": 8, "height": 12 },
-        { "name": "", "width": 12, "height": 18 },
-        { "name": "", "width": 16, "height": 24 },
-        { "name": "", "width": 20, "height": 30 },
-        { "name": "", "width": 24, "height": 36 },
-    ]
+        { "name": "Wallet", "width": 2, "height": 3, unit: "in" },
+        { "name": "Enprint", "width": 3.5, "height": 5, unit: "in" },
+        { "name": "", "width": 5, "height": 7, unit: "in" },
+        { "name": "", "width": 8, "height": 12, unit: "in" },
+        { "name": "", "width": 12, "height": 18, unit: "in" },
+        { "name": "", "width": 16, "height": 24, unit: "in" },
+        { "name": "", "width": 20, "height": 30, unit: "in" },
+        { "name": "", "width": 24, "height": 36, unit: "in" },
+    ],
 },
 ];
 
@@ -161,8 +161,8 @@ function tree(data) {
                         maxWidth = Math.min(el.width / 10, el.width / el.height * 136);
                     }
 
-                    shape.style.width = maxWidth + 'px';
-                    shape.style.height = maxHeight + 'px';
+                    shape.style.width = maxWidth + el.unit;
+                    shape.style.height = maxHeight + el.unit;
 
 
                     var shapeContainer = document.createElement("div");
@@ -178,7 +178,7 @@ function tree(data) {
                     var content = document.createElement("div");
 
                     title.appendChild(document.createTextNode(`${el.name}`));
-                    content.appendChild(document.createTextNode(`${el.width} x ${el.height} px`));
+                    content.appendChild(document.createTextNode(`${el.width} x ${el.height} ${el.unit}`));
 
                     info.appendChild(title)
                     info.appendChild(content)
