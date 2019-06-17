fda6ef2d5dee0f6c9a146eab3609ad11bf87b62f
Greg Herbowicz <herbowicz@gmail.com>
2019-06-17 06:10:19 +0200

    with www inside div

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
 let sizes = [{
     "Social": [
-        { "name": "FB Page Cover", "width": 1640, "height": 664, unit: "px" },
+        { "name": "FB Profile Picture", "width": 180, "height": 180, unit: "px" },
+        { "name": "FB Cover Photo", "width": 820, "height": 312, unit: "px" },
         { "name": "FB Shared Image", "width": 1200, "height": 630, unit: "px" },
+
+        { "name": "FB Page Cover", "width": 1640, "height": 664, unit: "px" },
+        { "name": "FB Group Header", "width": 1640, "height": 856, unit: "px" },
         { "name": "FB Event Image", "width": 1920, "height": 1080, unit: "px" },
 
-        { "name": "FB Group Header", "width": 1640, "height": 856, unit: "px" },
-        { "name": "Instagram", "width": 1080, "height": 1080, unit: "px" },
-        { "name": "YouTube Profile", "width": 800, "height": 800, unit: "px" },
-
-        { "name": "YouTube Cover", "width": 2560, "height": 1440, unit: "px" },
         { "name": "Twitter Profile", "width": 400, "height": 400, unit: "px" },
-        { "name": "Twitter Cover", "width": 1500, "height": 500, unit: "px" }
+        { "name": "Twitter Cover", "width": 1500, "height": 500, unit: "px" },
+
+        { "name": "Instagram", "width": 1080, "height": 1080, unit: "px" },
+        { "name": "Instagram Stories", "width": 1080, "height": 1920, unit: "px" },
+
+        { "name": "LinkedIn Profile", "width": 400, "height": 400, unit: "px" },
+        { "name": "Linkedin Background", "width": 1584, "height": 396, unit: "px" },
+
+        { "name": "YouTube Profile", "width": 800, "height": 800, unit: "px" },
+        { "name": "YouTube Cover", "width": 2560, "height": 1440, unit: "px" },
+
+
+        { "name": "Pinterest Profile", "width": 165, "height": 165, unit: "px" },
+        { "name": "Tumblr Image Posts", "width": 500, "height": 750, unit: "px" },
+        { "name": "Github Profile", "width": 500, "height": 500, unit: "px" },
     ],
 },
 {
@@ -164,10 +177,25 @@ function tree(data) {
                     shape.style.width = maxWidth + el.unit;
                     shape.style.height = maxHeight + el.unit;
 
+                    //   <object type="text/html" data="http://validator.w3.org/"></object>
+
+                    var object = document.createElement("object");
+                    object.setAttribute("type", "text/html");
+                    object.setAttribute("data", "http://murzyni.com/");
+                    object.style.width = "2500px";
+                    object.style.height = maxHeight + el.unit;
+                    object.style.transform = "scale(.2)";
+                    object.style.transformOrigin = "top left";
+
+                    object.style.height = maxHeight * 5 + el.unit;
+                    object.style.resize = 'both';
 
                     var shapeContainer = document.createElement("div");
                     shapeContainer.classList.add('shape-container');
+
                     shapeContainer.appendChild(shape);
+                    shape.appendChild(object);
+
 
                     var info = document.createElement("div");
                     info.classList.add('info');
